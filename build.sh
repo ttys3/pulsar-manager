7b78d482700ed65da01aedf78eb11b03bdd36c75
ttyS3 <ttys3.rust@gmail.com>
2022-05-12 15:43:00 +0800

    build: refine build.sh, add .gradle cache

--- build.sh
+++ build.sh
@@ -5,15 +5,14 @@ set -eou pipefail
 rm -rf .gradle
 rm -rf front-end/.gradle
 
+mkdir -p .gradle/caches
+
 # generate build/distributions/pulsar-manager.tar
-podman run --rm -ti -v $(pwd):/build -w /build openjdk:8-jdk sh -c './gradlew build -x test'
+podman run --rm -ti -v "$(pwd)/.gradle/caches":/root/.gradle/caches -v "$(pwd)":/build -w /build openjdk:8-jdk sh -c './gradlew build -x test'
 
 ls -l build/distributions
 
-cd front-end
-
-podman run --rm -ti -v $(pwd):/build -w /build node:12.22.10-buster-slim sh -c 'npm install && npm rebuild node-sass && npm run build:prod'
-cd ..
+podman run --rm -ti -v "$(pwd)/front-end":/build -w /build node:12.22.10-buster-slim sh -c 'npm install && npm rebuild node-sass && npm run build:prod'
 
 podman build -t pulsar-manager:v0.3.0-candidate-3 -f ./docker/Dockerfile .
 podman push pulsar-manager:v0.3.0-candidate-3 docker.io/80x86/pulsar-manager:v0.3.0-candidate-3
